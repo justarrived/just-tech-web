5a03803da39f9264be631aa3a704e80d55f41c85
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2019-02-06 21:55:59 +0300

    Adjust grey color same as docs

--- tailwind.js
+++ tailwind.js
@@ -50,7 +50,7 @@ let colors = {
   'grey-darkest': '#3d4852',
   'grey-darker': '#606f7b',
   'grey-dark': '#8795a1',
-  'grey': '#b8c2cc',
+  'grey': '#383F4E', // same as design documents
   'grey-light': '#dae1e7',
   'grey-lighter': '#f1f5f8',
   'grey-lightest': '#f8fafc',
