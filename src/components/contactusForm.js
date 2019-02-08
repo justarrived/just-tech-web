e13f59e96424b7cfba3b6fbbed69b2fc15a20309
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2019-02-08 19:58:08 +0300

    Make contact form responsive

--- src/components/contactusForm.js
+++ src/components/contactusForm.js
@@ -3,8 +3,8 @@ import "./contactus.css"
 
 const ContactusForm = () => ( 
 <div className="grid">
- <div className="flex p-10 bg-grey">
-    <div className="w-1/2 column pl-20 pr-20">
+ <div className="flex flex-wrap lg:p-10 md:p-8 p-2 bg-grey pt-10 py-3">
+    <div className="lg:w-1/2 w-full lg:pl-20 lg:pr-20 px-4 py-3">
       <div className="text-white">
       <p>Vi tillhandahåller topptalang inom mjukvaruutveckling </p>
       <ul>
@@ -16,34 +16,34 @@ const ContactusForm = () => (
       <p><span>andreas@justarrived.se </span>0733-86 86 56</p>
       </div>
     </div>
-    <div className="w-1/2 pl-20 pr-20">
+    <div className="lg:w-1/2 w-full lg:pl-20 lg:pr-20 pl-2 pr-2">
       <form>
         <div className="flex flex-wrap justify-end">
-          <div className="w-1/2 "> 
+          <div className="lg:w-1/2 md:w-1/2 w-full"> 
             <div className="sixteen wide column single-input-container">
               <div className="p-2">
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline bg-grey focus:bg-white" type="text" placeholder="Namn" />
+                <input className="shadow appearance-none border rounded w-full py-2 lg:px-3 px-2 text-grey-darker leading-tight focus:outline-none focus:shadow-outline bg-grey focus:bg-white" type="text" placeholder="Namn" />
               </div>
               <div className="p-2">
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline bg-grey focus:bg-white" type="text" placeholder="Foretag" />
+                <input className="shadow appearance-none border rounded w-full py-2 lg:px-3 px-2 text-grey-darker leading-tight focus:outline-none focus:shadow-outline bg-grey focus:bg-white" type="text" placeholder="Foretag" />
               </div>
               <div className="p-2">
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline bg-grey focus:bg-white" type="text" placeholder="Epost" />
+                <input className="shadow appearance-none border rounded w-full py-2 lg:px-3 px-2 text-grey-darker leading-tight focus:outline-none focus:shadow-outline bg-grey focus:bg-white" type="text" placeholder="Epost" />
               </div>
               <div className="p-2">
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline bg-grey focus:bg-white" type="text" placeholder="Telefonnummer" />
+                <input className="shadow appearance-none border rounded w-full py-2 lg:px-3 px-2 text-grey-darker leading-tight focus:outline-none focus:shadow-outline bg-grey focus:bg-white" type="text" placeholder="Telefonnummer" />
               </div>
             </div>
           </div>
-          <div className="w-1/2"> 
+          <div className="lg:w-1/2 md:w-1/2 w-full"> 
               <div className="p-2 h-full">
                 <textarea 
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline h-full bg-grey focus:bg-white"
+                  className="shadow appearance-none border rounded w-full py-2 lg:px-3 px-2 text-grey-darker leading-tight focus:outline-none focus:shadow-outline h-full bg-grey focus:bg-white"
                   type="text" value="" name="description" id="mce-DESCRIPTION" required placeholder="What do you need help with?"></textarea>
             </div>
           </div>
           <div className="w-20 p-2"> 
-            <button className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline">Skicka</button>
+            <button className="shadow appearance-none border rounded w-full py-2 lg:px-3 px-2 text-grey-darker leading-tight focus:outline-none focus:shadow-outline">Skicka</button>
           </div>
         </div>
     </form>
