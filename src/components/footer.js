f87cf8e8da83b68b1a9fcf117a94e1712975e320
Pontus Alexander <pomle@users.noreply.github.com>
2019-02-11 13:42:46 +0100

    Remove text from footer.
    Wrong typeface.
    
    Add back once we have proper format and type.

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,6 @@ import logo from "../images/logo.png"
 const Footer = () => (
     <footer className="bg-grey text-center">
         <span><img className="w-6 m-0" src={logo} /></span>
-        <span className="text-white font-bold">Just Tech</span>
     </footer>
 )
 
