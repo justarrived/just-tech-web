6d4e1d9dd964e9de3c300ea04c6845c97792291f
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2019-02-14 13:55:02 +0100

    Improve gradient similarity to the design

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "./index.css"
 
-import landingImg from "../images/landing1.png"
+import landingImg from "../images/landing2.png"
 import logo from "../images/logo.png"
 import ContactUsForm from "../components/contactusForm"
 import PageBreaker from "../components/pageBreaker";
@@ -14,11 +14,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`justtech`, `application`]} />
     <Parallax
-            blur={1}
-            bgImage={landingImg}
-            bgImageAlt="tech"
-            strength={400} 
-        >
+      blur={1}
+      bgImage={landingImg}
+      bgImageAlt="tech"
+      strength={400}
+    >
       <div className="image-shadow text-center py-32 text-white" >
         <img src={logo} />
         <h1 className="m-0"> Just Tech </h1>
@@ -26,7 +26,7 @@ const IndexPage = () => (
         <p className="m-0"> inom IT med arbetsmarknaden </p>
         <p className="m-0"> Start Q1 2019 </p>
       </div>
-    {/* </div> */}
+      {/* </div> */}
     </Parallax>
     <PageBreaker />
     <ContactUsForm />
