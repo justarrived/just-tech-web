66d4ec889510821929e14c8a30af5d3180d65622
Pontus Persson <pontus.alexander@gmail.com>
2019-02-20 15:43:32 +0100

    Tweak page breaker.

--- src/components/pageBreaker.js
+++ src/components/pageBreaker.js
@@ -5,8 +5,9 @@ import bigLogo from "../images/full-logo.png"
 const PageBreaker = () => (
     <div className="container mx-auto text-center">
         <p className="italic m-0 pt-3"> En del av </p>
-        <img className="fill-current h-8 w-9 mr-2 -mb-1" width="auto" height="auto" src={bigLogo} />
-        <p className="italic m-0 pb-3"> justarrived.se </p>
+        <a href="https://justarrived.se/">
+            <img className="fill-current h-8 w-9 mr-2 mb-1" width="auto" height="auto" src={bigLogo} />
+        </a>
     </div>
 )
 
