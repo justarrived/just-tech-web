c36a1d82647baf28dab3afdb0f78e29aaa53da39
Pontus Persson <pontus.alexander@gmail.com>
2019-02-20 12:54:28 +0100

    Add Value Proposition.

---
+++ src/components/upsell.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import bigLogo from "../images/full-logo.png"
+
+const Upsell = () => (
+    <div className="mx-auto text-center bg-black p-6">
+        <h1 className="text-white leading-normal">
+            We Help<br/>
+            You Hire Top Talent and<br/>
+            Diversify your Teams
+        </h1>
+    </div>
+)
+
+export default Upsell
