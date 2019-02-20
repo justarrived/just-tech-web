d95000356ce2dbae25ef780b2a7492ccfed47123
Pontus Persson <pontus.alexander@gmail.com>
2019-02-20 14:42:52 +0100

    Add tech section.

---
+++ src/components/tech.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import bigLogo from "../images/full-logo.png"
+
+const Tech = () => (
+    <div className="mx-auto text-center bg-black p-10">
+        <h1 className="text-4xl text-white leading-normal">
+            Våra konsulter<br/>
+            är experter inom<br/>
+            Backend, Frontend, och Mobile
+        </h1>
+    </div>
+)
+
+export default Tech
