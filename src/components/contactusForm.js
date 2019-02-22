3e6a6380a31b721c927527e438c82aaa5d79ea31
Pontus Alexander <pomle@users.noreply.github.com>
2019-02-22 10:36:05 +0100

    Use Upsales iframe form. (#29)
    * Use Upsales iframe form.

--- src/components/contactusForm.js
+++ src/components/contactusForm.js
@@ -2,41 +2,27 @@ import React from "react"
 
 
 const ContactusForm = () => (
-  <div className="grid bg-black">
+  <div className="grid bg-black text-white">
     <div className="flex flex-wrap lg:p-10 lg:pb-0 md:p-8 p-2 pt-10 py-3">
       <div className="lg:w-1/2 w-full lg:pl-20 lg:pr-20 px-4 py-3">
-        <div className="text-white">
+        <div>
           <p className="m-0 font-bold"> För mer information kontakta Andreas König:</p>
           <p className="m-0"><span>andreas@justarrived.se</span> | 0733-86 86 56</p>
         </div>
       </div>
-      <div className="lg:w-1/2 w-full lg:pl-20 lg:pr-20 pl-2 pr-2">
-        <form data-netlify="true" name="contactus" method="POST">
-          <input type="hidden" name="form-name" value="contactus" />
-          <div className="flex flex-wrap justify-end">
-            <div className="lg:w-1/2 md:w-1/2 w-full">
-              <div className="sixteen wide column single-input-container">
+      <div className="lg:w-1/2 w-full lg:pl-20 lg:pr-20 pl-2 py-3">
+        <p className="m-0 font-bold"> Vill du att vi ringer upp dig?</p>
+        <p>Skriv in era detaljer så tar vi kontakt med er.</p>
 
-                {["Namn", "Foretag", "Epost", "Telefonnummer"].map(name =>
-                  <div key={`${name}-wrapper`} className="p-2">
-                    <input className="comp-form-input" type="text" placeholder={name} name={name} />
-                  </div>
-                )}
-
-              </div>
-            </div>
-            <div className="lg:w-1/2 md:w-1/2 w-full">
-              <div className="p-2 h-full">
-                <textarea
-                  className="comp-form-input h-full"
-                  type="text" name="description" id="mce-DESCRIPTION" required placeholder="Beskriv gärna kort ditt personalbehov"></textarea>
-              </div>
-            </div>
-            <div className="w-20 p-2">
-              <button className="comp-form-input" type="submit">Skicka</button>
-            </div>
-          </div>
-        </form>
+        <div className="-m-4">
+          <iframe
+            title="Upsales Contact Form"
+            src="https://pages.upsales.com/9084u0a61f381e979430c882e0881569e6eee-frame"
+            width="360"
+            height="529"
+            style={{border: 0}}>
+          </iframe>
+        </div>
       </div>
     </div>
   </div>
