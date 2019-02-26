2050f7d864673fe32a4fd6665ea76748baae09d2
Pontus Persson <pontus.alexander@gmail.com>
2019-02-26 13:15:43 +0100

    Even out padding on contact form.

--- src/components/contactusForm.js
+++ src/components/contactusForm.js
@@ -4,11 +4,9 @@ import React from "react"
 const ContactusForm = () => (
   <div className="grid bg-black text-white">
     <div className="flex flex-wrap lg:p-10 lg:pb-0 md:p-8 p-2 pt-10 py-3">
-      <div className="lg:w-1/2 w-full lg:pl-20 lg:pr-20 px-4 py-3">
-        <div>
-          <p className="m-0 font-bold">För mer information kontakta Andreas König:</p>
-          <p className="m-0"><span>andreas@justarrived.se</span> | 0733-86 86 56</p>
-        </div>
+      <div className="lg:w-1/2 w-full lg:pl-20 lg:pr-20 pl-2 py-3">
+        <p className="m-0 font-bold">För mer information kontakta Andreas König:</p>
+        <p className="m-0"><span>andreas@justarrived.se</span> | 0733-86 86 56</p>
       </div>
       <div className="lg:w-1/2 w-full lg:pl-20 lg:pr-20 pl-2 py-3">
         <p className="m-0 font-bold">Vill du att vi ringer upp dig?</p>
