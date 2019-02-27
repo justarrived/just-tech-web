0906a307abdf789baf552728b2537edc2e99c3a4
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2019-02-27 17:24:36 +0100

    Fix some bug in page breaker

--- src/components/pageBreaker.js
+++ src/components/pageBreaker.js
@@ -3,7 +3,7 @@ import React from "react"
 import bigLogo from "../images/full-logo.png"
 
 const PageBreaker = () => (
-    <div className="container mx-auto text-center">
+    <div className="text-center bg-white">
         <p className="italic m-0 pt-3"> En del av </p>
         <a href="https://justarrived.se/">
             <img className="fill-current h-8 w-9 mr-2 mb-1" alt="Just Arrived logo" width="auto" height="auto" src={bigLogo} />
