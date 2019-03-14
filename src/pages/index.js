2ed0bfdea5d1e09f4d0b80d6617ea4cf6c2a3076
Minoo Senobari <minoo.senobari@hotmail.com>
2019-03-14 10:52:07 +0100

    font fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
-import React from "react"
-import { Parallax } from "react-parallax"
+import React from "react";
+import { Parallax } from "react-parallax";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import "./index.css"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import "./index.css";
 
-import landingImg from "../images/landing.png"
-import computer from "../images/blake-connally-373084-unsplash-1280.jpg"
-import engineers from "../images/andrew-neel-237802-unsplash-1280.jpg"
-import code from "../images/focused-female-engineer-working-at-laptop.jpg"
-import logoIcon from "../images/logo.png"
-import logoText from "../images/justtech-text-logo.svg"
-import ContactUsForm from "../components/contactusForm"
+import landingImg from "../images/landing.png";
+import computer from "../images/blake-connally-373084-unsplash-1280.jpg";
+import engineers from "../images/andrew-neel-237802-unsplash-1280.jpg";
+import code from "../images/focused-female-engineer-working-at-laptop.jpg";
+import logoIcon from "../images/logo.png";
+import logoText from "../images/justtech-text-logo.svg";
+import ContactUsForm from "../components/contactusForm";
 import PageBreaker from "../components/pageBreaker";
 import WhoWeAre from "../components/whoWeAre";
 
@@ -26,25 +26,29 @@ const IndexPage = () => (
       bgImageAlt="tech"
       strength={400}
     >
-      <div className="image-shadow text-center py-32 text-white" >
-        <img src={logoIcon} alt="JustTech Logo Icon"/><br/>
-        <img src={logoText} alt="JustTech Logo Text" className="m-4"/>
+      <div className="image-shadow text-center py-32 text-white">
+        <img src={logoIcon} alt="JustTech Logo Icon" />
+        <br />
+        <img src={logoText} alt="JustTech Logo Text" className="m-4" />
 
         <p className="m-4">
-          Matchar utrikesfödda topptalanger<br />
+          Matchar utrikesfödda topptalanger
+          <br />
           inom IT med arbetsmarknaden
         </p>
 
-        <div class="m-auto bg-white h-2 w-6"></div>
+        <div class="m-auto bg-white h-2 w-6" />
       </div>
     </Parallax>
 
     <div className="comp-section-container">
-        <h1 className="comp-section-header">
-            Vi hjälper er<br />
-            hitta topptalang och<br />
-            diversifiera era team
-        </h1>
+      <h1 className="comp-section-header text-base sm:text-lg md:text-lg lg:text-2xl xl:text-3xl">
+        Vi hjälper er
+        <br />
+        hitta topptalang och
+        <br />
+        diversifiera era team
+      </h1>
     </div>
 
     <Parallax
@@ -57,11 +61,13 @@ const IndexPage = () => (
     </Parallax>
 
     <div className="comp-section-container">
-        <h1 className="comp-section-header">
-            Våra konsulter<br />
-            är experter inom<br />
-            Frontend, Backend och Mobile
-        </h1>
+      <h1 className="comp-section-header text-base sm:text-lg md:text-lg lg:text-2xl xl:text-3xl">
+        Våra konsulter
+        <br />
+        är experter inom
+        <br />
+        Frontend, Backend och Mobile
+      </h1>
     </div>
 
     <Parallax
@@ -74,12 +80,15 @@ const IndexPage = () => (
     </Parallax>
 
     <div className="comp-section-container">
-        <h1 className="comp-section-header">
-            Vi arbetar med<br />
-            tekniker och språk som<br />
-            JavaScript, Python, Java, PHP, Ruby, C#<br />
-            <small>React, React Native, Angular, Vue, Node.js, Spring</small>
-        </h1>
+      <h1 className="comp-section-header text-base sm:text-lg md:text-lg lg:text-2xl xl:text-3xl">
+        Vi arbetar med
+        <br />
+        tekniker och språk som
+        <br />
+        JavaScript, Python, Java, PHP, Ruby, C#
+        <br />
+        <small>React, React Native, Angular, Vue, Node.js, Spring</small>
+      </h1>
     </div>
 
     <Parallax
@@ -92,15 +101,15 @@ const IndexPage = () => (
     </Parallax>
 
     <div className="comp-section-container">
-        <h1 className="comp-section-header">
-            Tryggt och säkert för alla!
-        </h1>
+      <h1 className="comp-section-header text-base sm:text-lg md:text-lg lg:text-2xl xl:text-3xl">
+        Tryggt och säkert för alla!
+      </h1>
     </div>
 
     <ContactUsForm />
 
     <WhoWeAre />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
