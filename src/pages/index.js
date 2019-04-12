a10ebe108a37440289691e3b9e948535cba062c7
Pontus Alexander <pontus.alexander@gmail.com>
2019-04-12 09:47:59 +0200

    Separate Layouts.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Parallax } from "react-parallax";
 
-import Layout from "../components/layout";
+import Layout from "../components/layouts/Splash";
 import SEO from "../components/seo";
 import "./index.css";
 
