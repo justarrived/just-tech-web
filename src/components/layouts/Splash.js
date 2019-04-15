00d2e484771505605048b9ac3b9c423ae8c5dc59
Pontus Alexander <pontus.alexander@gmail.com>
2019-04-15 16:44:46 +0200

    Remove unused prop.

--- src/components/layouts/Splash.js
+++ src/components/layouts/Splash.js
@@ -1,32 +1,18 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 
 import Menu from "../menu"
 import Footer from "../footer"
 import "./Splash.css"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div id="outer-container" style={{ height: '100%' }}>
-        <Menu siteTitle={data.site.siteMetadata.title} />
-        <main id="page-wrap">
-          {children}
-        </main>
-        <Footer />
-      </div>
-    )}
-  />
+  <div id="outer-container" style={{ height: '100%' }}>
+    <Menu/>
+    <main id="page-wrap">
+      {children}
+    </main>
+    <Footer />
+  </div>
 )
 
 Layout.propTypes = {
