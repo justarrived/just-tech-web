91b8e9301c139ccb243554e68a27a7310b56c1fd
Pontus Alexander <pontus.alexander@gmail.com>
2019-04-25 09:49:12 +0200

    Change logo and theming.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,7 @@
 import React from "react";
 
-import logo from "../images/justarrived-heart.svg";
-
 const Footer = () => (
-  <footer className="bg-grey text-center">
-    <span>
-      <img className="w-10 m-4" src={logo} alt="JustTech <3" />
-    </span>
-  </footer>
+  <footer/>
 );
 
 export default Footer;
